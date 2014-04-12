a133b53483224a117de34961da3e5c6c9a92a4a5
rocktail <oldie.fiction+github@gmail.com>
2014-04-12 17:28:39 +0200

    added menu item in redmine top menu

--- init.rb
+++ init.rb
@@ -5,4 +5,7 @@ Redmine::Plugin.register :red_trip do
   version '0.0.1'
   url 'https://github.com/rocktail/red_trip'
   author_url 'https://github.com/rocktail'
+
+  menu :top_menu, :red_trip, { :controller => 'red_trip', :action => 'index' }, :caption => 'Business trips'
+  
 end
